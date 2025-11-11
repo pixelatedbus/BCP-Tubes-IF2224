a6332b696e00f9df38e77609fbb7d6ebe34c4ba5
Henshou <gerrardgrs@gmail.com>
2025-11-11 07:07:20 +0700

    feat node class

---
+++ src/parser/model/node.py
@@ -0,0 +1,14 @@
+class ParseNode:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+    
+    def add_child(self, child):
+        if child:
+            self.children.append(child)
+
+    def to_string(self, indent=0):
+        result = " " * indent + self.name + "\n"
+        for child in self.children:
+            result += child.to_string(indent + 2)
+        return result
