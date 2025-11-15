2d4e1b1f3206a44200c424cb6165eb5172f4ecf3
Julius <juliusarthur2005@gmail.com>
2025-11-15 12:44:20 +0700

    feat: additive and multiplicative operator

---
+++ src/parser/model/expression/additive.py
@@ -0,0 +1,12 @@
+from src.parser.model.node import ParseNode
+
+def parse_additive_expression(parent):
+    token = parent.current_token()
+
+    if token[0] == 'ARITHMETIC_OPERATOR' and token[1] in ['+', '-']:
+        parent.advance()
+        return ParseNode(f'ARITHMETIC_OPERATOR({token[1]})')
+
+    if token[0] == 'LOGICAL_OPERATOR' and token[1] in ['atau']:
+        parent.advance()
+        return ParseNode(f'LOGICAL_OPERATOR({token[1]})')
